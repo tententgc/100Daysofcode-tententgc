30fd97473487256ff88c07e7e4abf6fede9be214
tententgc <nyman3527@gmail.com>
2022-02-06 12:58:55 +0700

    Update Printing.py

--- Days 1/Printing.py
+++ Days 1/Printing.py
@@ -1,2 +1,7 @@
-print("Day 1 - String Manipulation\nString Concatenation is done with the " +
-      " sign.\ne.g. print(\"Hello \" + \"world\")\nNew lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation\nString Concatenation is done with the " +
+#       " sign.\ne.g. print(\"Hello \" + \"world\")\nNew lines can be created with a backslash and n.")
+a = int(input())
+if 1 < a <10: 
+      print("a is between 1 and 10") 
+else:
+      print("a is not between 1 and 10")
